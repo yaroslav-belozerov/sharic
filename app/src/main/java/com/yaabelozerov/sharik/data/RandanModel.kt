12b47f947936c9ba27edc182ede2792e4354a1d7
Yaroslav Belozerov <yaabelozerov@edu.hse.ru>
2024-11-09 16:42:47 +0300

    randans model

---
+++ app/src/main/java/com/yaabelozerov/sharik/data/RandanModel.kt
@@ -0,0 +1,71 @@
+package com.yaabelozerov.sharik.data
+
+import com.squareup.moshi.Json
+
+typealias Randans = List<Randan>;
+
+data class Randan(
+    val id: String,
+    val name: String,
+    val activities: List<Activity>,
+    val debts: List<Debt>,
+    val users: List<User3>,
+    val isFinished: String,
+)
+
+data class Activity(
+    val id: String,
+    val name: String,
+    val sum: Long,
+    val pays: Pays,
+    val randan: String,
+    val owes: List<Owe>,
+)
+
+data class Pays(
+    val id: String,
+    val username: String,
+    val firstName: String,
+    val lastName: String,
+    @Json(name = "avatarURL")
+    val avatarUrl: String,
+)
+
+data class Owe(
+    val user: User,
+    val amount: Long,
+)
+
+data class User(
+    val id: String,
+    val username: String,
+    val firstName: String,
+    val lastName: String,
+    @Json(name = "avatarURL")
+    val avatarUrl: String,
+)
+
+data class Debt(
+    val id: String,
+    val user: User2,
+    val randan: Map<String, Any>,
+    val amount: Long,
+)
+
+data class User2(
+    val id: String,
+    val username: String,
+    val firstName: String,
+    val lastName: String,
+    @Json(name = "avatarURL")
+    val avatarUrl: String,
+)
+
+data class User3(
+    val id: String,
+    val username: String,
+    val firstName: String,
+    val lastName: String,
+    @Json(name = "avatarURL")
+    val avatarUrl: String,
+)
